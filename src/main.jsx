02004391d2323db5a3147a8b3bb1cfb918f9cb34
sathwik-001 <sathwik.nandu12@gmail.com>
2026-04-26 23:39:11 +0530

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/sathwik-react1">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
